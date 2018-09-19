53c17c23db67535efe20236276194e98ed552219
passok11 <muriloarrudap@gmail.com>
2018-09-19 18:31:40 -0300

    Add data persitence

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const chalk = require('chalk');
+const fs = require('fs');
 
 const success = chalk.green;
 const waiting = chalk.blue;
@@ -8,21 +9,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-// Should get from a file!
-const todos = [
+
+const template = `[
   {
-    isChecked: false,
-    text: 'first todo'
+    "isChecked": false,
+    "text": "Check me to test if is working! "
   },
   {
-    isChecked: true,
-    text: 'second todo'
-  },
-  {
-    isChecked: false,
-    text: 'third todo'
-  },
-];
+    "isChecked": true,
+    "text": "You can remove this template todo!"
+  }
+]`;
 
 function showTodos() {
   todos.forEach((todo, index)=> {
@@ -35,18 +32,7 @@ function askForATask(withHelp) {
   console.clear();
 
   if(withHelp) {
-    console.log(`
-      TODO LIST NODE CLI\n
-      Manager todos anytime using command line!\n
-      Every change will be saved in your system.\n
-      usage: 'command [arguments]'\n
-
-      add - add a new todo: 'add my new task'\n
-      check - check itens: 'check 0 2'. this will check the first item and the third.\n
-      remove - remove items: 'remove 0 1'. this will remove the first two items.\n
-
-      > PRESS ENTER TO CONTINUE < \n
-    `)
+    showHelp();
   } else {
     showTodos();
     console.log('type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ');
@@ -79,10 +65,19 @@ function removeTodos(ids){
     }
   });
 }
-function showHelp(err) {
-  if (err) {
-    console.log('invalid input')
-  }
+function showHelp() {
+  console.log(`
+  ${chalk.bgGreen('TODO LIST NODE CLI')}\n
+  Manager todos anytime using command line!\n
+  Every change will be saved in your system.\n
+  usage: 'command [arguments]' - the arguments are space separated!\n
+
+  add - add a new todo. Example ${chalk.inverse('add my new task')}\n
+  check - checkmark the  items. Example: ${chalk.inverse('check 0 2')}. this will check the first item and the third.\n
+  remove - remove items from the list. Example ${chalk.inverse('remove 0 1')}. this will remove the first two items.\n
+  you can use the initial letter of each command for a shortcut\n
+  > PRESS ENTER TO CONTINUE < \n
+`)
 }
 function checkTask(answer, args) {
   let help = false;
@@ -113,6 +108,42 @@ function checkTask(answer, args) {
       help = false;
 
   }
+  saveData();
   askForATask(help);
 }
-askForATask(false);
+// try to read data from a json file
+function loadFile() {
+  try{
+    todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
+    askForATask(false);
+  } catch (err){
+    if (err.code = 'ENOENT'){
+      console.log('Todo file not found. do you want generate a new one? (Y/n)');
+      rl.question('> ', (answer) => {
+        switch(answer) {
+            case 'y':
+            case 'Y':
+            case 'YES':
+            case 'yes':
+              fs.writeFileSync('todos.json', template, 'utf8');
+              todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
+              askForATask(false);
+              break;
+            default:
+              console.log('Exiting...');
+              process.exit(0);
+        }
+       });
+
+    } else {
+      console.log(err);
+      process.exit(0);
+    }
+  }
+}
+function saveData() {
+  fs.writeFileSync('todos.json', JSON.stringify(todos), 'utf8');
+}
+let todos;
+loadFile();
+
