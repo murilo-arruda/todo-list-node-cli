5a95de4f7b7ad2a6e10f2f4cb805566d1864bbb1
Mikael Midtgaard <mikaelmidt@gmail.com>
2018-10-01 19:40:06 +0200

    Formatted code with prettier, added precommit hook so all future code is formatted as well

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const readline = require('readline');
-const chalk = require('chalk');
-const fs = require('fs');
+const readline = require("readline");
+const chalk = require("chalk");
+const fs = require("fs");
 
 const success = chalk.green;
 const waiting = chalk.blue;
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
 const template = `[
@@ -22,44 +22,46 @@ const template = `[
 ]`;
 
 function showTodos() {
-  todos.forEach((todo, index)=> {
+  todos.forEach((todo, index) => {
     const color = todo.isChecked ? success : waiting;
-    console.log(color(`${index} - [${todo.isChecked ? 'X' : ' ' }] ${todo.text}`))
+    console.log(
+      color(`${index} - [${todo.isChecked ? "X" : " "}] ${todo.text}`)
+    );
   });
 }
 
 function askForATask(withHelp) {
   console.clear();
 
-  if(withHelp) {
+  if (withHelp) {
     showHelp();
   } else {
     showTodos();
-    console.log('type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ');
+    console.log("type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ");
   }
-  rl.question('> ', (answer) => {
-    [answer, ...args] = answer.split(' ');
+  rl.question("> ", answer => {
+    [answer, ...args] = answer.split(" ");
     checkTask(answer, args);
   });
 }
 function addTodo(text) {
-  if( text.length > 0){
+  if (text.length > 0) {
     todos.push({
       isChecked: false,
-      text,
+      text
     });
   }
 }
-function checkTodos(ids){
-  ids.forEach((id) => {
-    if (todos[id]){
+function checkTodos(ids) {
+  ids.forEach(id => {
+    if (todos[id]) {
       todos[id].isChecked = !todos[id].isChecked;
     }
   });
 }
-function removeTodos(ids){
+function removeTodos(ids) {
   ids.sort((a, b) => b - a);
-  ids.forEach((id) => {
+  ids.forEach(id => {
     if (todos[id]) {
       todos.splice(id, 1);
     }
@@ -67,74 +69,78 @@ function removeTodos(ids){
 }
 function showHelp() {
   console.log(`
-  ${chalk.bgGreen('TODO LIST NODE CLI')}\n
+  ${chalk.bgGreen("TODO LIST NODE CLI")}\n
   Manage your todos anytime using command line!\n
   Every change will be saved in your system.\n
   usage: 'command [arguments]' - the arguments are space separated!\n
 
-  ${chalk.green('add')} - add a new todo. Example ${chalk.inverse('add my new task')}\n
-  ${chalk.blue('check')} - checkmark the  items. Example: ${chalk.inverse('check 0 2')}. this will check the first item and the third.\n
-  ${chalk.red('remove')} - remove items from the list. Example ${chalk.inverse('remove 0 1')}. this will remove the first two items.\n
+  ${chalk.green("add")} - add a new todo. Example ${chalk.inverse(
+    "add my new task"
+  )}\n
+  ${chalk.blue("check")} - checkmark the  items. Example: ${chalk.inverse(
+    "check 0 2"
+  )}. this will check the first item and the third.\n
+  ${chalk.red("remove")} - remove items from the list. Example ${chalk.inverse(
+    "remove 0 1"
+  )}. this will remove the first two items.\n
   you can use the initial letter of each command for a shortcut\n
   > PRESS ENTER TO CONTINUE < \n
-`)
+`);
 }
 function checkTask(answer, args) {
   let help = false;
-  switch(answer){
-    case 'a':
-    case 'add':
-      addTodo(args.join(' '));
+  switch (answer) {
+    case "a":
+    case "add":
+      addTodo(args.join(" "));
       break;
-    case 'c':
-    case 'check':
+    case "c":
+    case "check":
       checkTodos(args);
       break;
-    case 'r':
-    case 'remove':
+    case "r":
+    case "remove":
       removeTodos(args);
       break;
-    case 'h':
-    case 'help':
+    case "h":
+    case "help":
       help = true;
       break;
-    case 'e':
-    case 'exit':
+    case "e":
+    case "exit":
       console.clear();
       rl.close();
       process.exit();
       break;
     default:
       help = false;
-
   }
   saveData();
   askForATask(help);
 }
 
 function loadFile() {
-  try{
-    todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
+  try {
+    todos = JSON.parse(fs.readFileSync("todos.json", "utf8"));
     askForATask(false);
-  } catch (err){
-    if (err.code = 'ENOENT'){
-      console.log('Todo file not found. do you want generate a new one? (Y/n)');
-      rl.question('> ', (answer) => {
-        switch(answer) {
-            case 'y':
-            case 'Y':
-            case 'YES':
-            case 'yes':
-              fs.writeFileSync('todos.json', template, 'utf8');
-              todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
-              askForATask(false);
-              break;
-            default:
-              console.log('Exiting...');
-              process.exit(0);
+  } catch (err) {
+    if ((err.code = "ENOENT")) {
+      console.log("Todo file not found. do you want generate a new one? (Y/n)");
+      rl.question("> ", answer => {
+        switch (answer) {
+          case "y":
+          case "Y":
+          case "YES":
+          case "yes":
+            fs.writeFileSync("todos.json", template, "utf8");
+            todos = JSON.parse(fs.readFileSync("todos.json", "utf8"));
+            askForATask(false);
+            break;
+          default:
+            console.log("Exiting...");
+            process.exit(0);
         }
-       });
-
+      });
     } else {
       console.log(err);
       process.exit(0);
@@ -142,8 +148,7 @@ function loadFile() {
   }
 }
 function saveData() {
-  fs.writeFileSync('todos.json', JSON.stringify(todos), 'utf8');
+  fs.writeFileSync("todos.json", JSON.stringify(todos), "utf8");
 }
 let todos;
 loadFile();
-
