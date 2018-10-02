1529fdeed5bd96e34d89a690cca80b17dbb5d855
jackiecalapristi <jackie.lc11@gmail.com>
2018-10-01 21:11:44 -0700

    Fixing capitalization, adding options with alternative casing, cleaning up syntax

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const template = `[
   },
   {
     "isChecked": true,
-    "text": "You can remove this template todo!"
+    "text": "You can remove this template todo! "
   }
 ]`;
 
@@ -35,13 +35,14 @@ function askForATask(withHelp) {
     showHelp();
   } else {
     showTodos();
-    console.log('type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ');
+    console.log('Type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ');
   }
   rl.question('> ', (answer) => {
     [answer, ...args] = answer.split(' ');
     checkTask(answer, args);
   });
 }
+
 function addTodo(text) {
   if( text.length > 0){
     todos.push({
@@ -50,6 +51,7 @@ function addTodo(text) {
     });
   }
 }
+
 function checkTodos(ids){
   ids.forEach((id) => {
     if (todos[id]){
@@ -57,6 +59,7 @@ function checkTodos(ids){
     }
   });
 }
+
 function removeTodos(ids){
   if(ids[0].length === 3 && ids[0].includes('-')) {
     const [startRemoval, stopRemoval] = ids[0].split('-');
@@ -74,17 +77,18 @@ function removeTodos(ids){
     });
   }
 }
+
 function showHelp() {
   console.log(`
   ${chalk.bgGreen('TODO LIST NODE CLI')}\n
   Manage your todos anytime using command line!\n
   Every change will be saved in your system.\n
-  usage: 'command [arguments]' - the arguments are space separated!\n
+  Usage: 'command [arguments]' - the arguments are space separated!\n
 
-  ${chalk.green('add')} - add a new todo. Example ${chalk.inverse('add my new task')}\n
-  ${chalk.blue('check')} - checkmark the  items. Example: ${chalk.inverse('check 0 2')}. this will check the first item and the third.\n
-  ${chalk.red('remove')} - remove items from the list. Example ${chalk.inverse('remove 0 1')}. this will remove the first two items.\n
-  you can use the initial letter of each command for a shortcut\n
+  ${chalk.green('add')} - Add a new todo. Example ${chalk.inverse('add my new task')}\n
+  ${chalk.blue('check')} - Checkmark the  items. Example: ${chalk.inverse('check 0 2')}. This will check the first item and the third.\n
+  ${chalk.red('remove')} - Remove items from the list. Example ${chalk.inverse('remove 0 1')}. This will remove the first two items.\n
+  You can use the initial letter of each command for a shortcut.\n
   > PRESS ENTER TO CONTINUE < \n
 `)
 }
@@ -92,23 +96,33 @@ function checkTask(answer, args) {
   let help = false;
   switch(answer){
     case 'a':
+    case 'A':
     case 'add':
+    case 'Add':
       addTodo(args.join(' '));
       break;
     case 'c':
+    case 'C':
     case 'check':
+    case 'Check':
       checkTodos(args);
       break;
     case 'r':
+    case 'R':
     case 'remove':
+    case 'Remove':
       removeTodos(args);
       break;
     case 'h':
+    case 'H':
     case 'help':
+    case 'Help':
       help = true;
       break;
     case 'e':
+    case 'E':
     case 'exit':
+    case 'Exit':
       console.clear();
       rl.close();
       process.exit();
@@ -127,20 +141,21 @@ function loadFile() {
     askForATask(false);
   } catch (err){
     if (err.code = 'ENOENT'){
-      console.log('Todo file not found. do you want generate a new one? (Y/n)');
+      console.log('Todo file not found. Do you want generate a new one? (Y/n)');
       rl.question('> ', (answer) => {
         switch(answer) {
-            case 'y':
-            case 'Y':
-            case 'YES':
-            case 'yes':
-              fs.writeFileSync('todos.json', template, 'utf8');
-              todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
-              askForATask(false);
-              break;
-            default:
-              console.log('Exiting...');
-              process.exit(0);
+          case 'y':
+          case 'Y':
+          case 'YES':
+          case 'yes':
+          case 'Yes':
+            fs.writeFileSync('todos.json', template, 'utf8');
+            todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
+            askForATask(false);
+            break;
+          default:
+            console.log('Exiting...');
+            process.exit(0);
         }
        });
 
@@ -150,9 +165,10 @@ function loadFile() {
     }
   }
 }
+
 function saveData() {
   fs.writeFileSync('todos.json', JSON.stringify(todos), 'utf8');
 }
+
 let todos;
 loadFile();
-
