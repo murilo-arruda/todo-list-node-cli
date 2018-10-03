e47162660fc32014e5d76dacf3b701077cb80afa
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-10-03 16:11:32 +0100

    Show timestamp reflecting latest activity on a todo

--- index.js
+++ index.js
@@ -13,18 +13,29 @@ const rl = readline.createInterface({
 const template = `[
   {
     "isChecked": false,
-    "text": "Check me to test if is working! "
+    "text": "Check me to test if is working! ",
+    "lastActivity": "created",
+    "lastUpdated": ${Date.now()}
   },
   {
     "isChecked": true,
-    "text": "You can remove this template todo!"
+    "text": "You can remove this template todo!",
+    "lastActivity": "checked",
+    "lastUpdated": ${Date.now()}
   }
 ]`;
+
+function formatTodoTime(time) {
+  const date = new Date(time);
+  return chalk.grey(
+    `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+  );
+}
 
 function showTodos() {
   todos.forEach((todo, index)=> {
     const color = todo.isChecked ? success : waiting;
-    console.log(color(`${index} - [${todo.isChecked ? 'X' : ' ' }] ${todo.text}`))
+    console.log(color(`${index} - [${todo.isChecked ? 'X' : ' ' }] ${todo.text}\t ${todo.lastActivity} ${formatTodoTime(todo.lastUpdated)}`))
   });
 }
 
@@ -47,6 +58,8 @@ function addTodo(text) {
     todos.push({
       isChecked: false,
       text,
+      lastActivity: 'created',
+      lastUpdated: Date.now(),
     });
   }
 }
@@ -54,6 +67,8 @@ function checkTodos(ids){
   ids.forEach((id) => {
     if (todos[id]){
       todos[id].isChecked = !todos[id].isChecked;
+      todos[id].lastActivity = 'checked';
+      todos[id].lastUpdated = Date.now();
     }
   });
 }
